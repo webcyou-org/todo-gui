c649220c0ee655341e9f6edecf5dcbb96079380b
panicdragon <d.takayama.web@gmail.com>
2024-09-14 04:04:02 +0900

    feat: 🎸 add QT app

---
+++ C++/QT/main.cpp
@@ -0,0 +1,9 @@
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QLabel *label = new QLabel("Hello World!");
+    label->show();
+    return app.exec();
+}
