37389e4c4d0314928721e0b19171bd37f267ad43
panicdragon <d.takayama.web@gmail.com>
2026-05-04 12:22:38 +0900

    refactor: add flutter files

--- Dart/Flutter/todo/lib/main.dart
+++ Dart/Flutter/todo/lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = WindowOptions(
+    size: Size(800, 600),
+    center: true,
+    skipTaskbar: false,
+    // titleBarStyle: TitleBarStyle.hidden,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(ToDo());
 }
 
