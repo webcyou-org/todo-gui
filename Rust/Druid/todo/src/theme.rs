751367410de8e90ce1630827d4a9f97885c9987f
panicdragon <d.takayama.web@gmail.com>
2026-05-04 19:37:59 +0900

    feat: finish Druid app

---
+++ Rust/Druid/todo/src/theme.rs
@@ -0,0 +1,10 @@
+use druid::Color;
+
+pub const BG: Color             = Color::rgb8(0x1C, 0x1C, 0x1C);
+pub const INPUT_BG: Color       = Color::rgb8(0x30, 0x30, 0x30);
+pub const ITEM_BG: Color        = Color::rgb8(0x2A, 0x2A, 0x2A);
+pub const ACCENT: Color         = Color::rgb8(0x5D, 0xC2, 0xAF);
+pub const TEXT_WHITE: Color     = Color::rgb8(0xFF, 0xFF, 0xFF);
+pub const TEXT_MAIN: Color      = Color::rgb8(0xD2, 0xD2, 0xD2);
+pub const TEXT_HINT: Color      = Color::rgb8(0x9B, 0x9B, 0x9B);
+pub const CHECKBOX_BORDER: Color = Color::rgb8(0xD9, 0xD9, 0xD9);
---
+++ Rust/Druid/todo/src/widget/mod.rs
@@ -0,0 +1,7 @@
+pub mod input;
+pub mod tabs;
+pub mod todo_item;
+
+pub use input::build_input;
+pub use tabs::build_tabs;
+pub use todo_item::make_todo_row;
